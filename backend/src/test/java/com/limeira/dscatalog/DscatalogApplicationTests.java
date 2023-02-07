6846876d38430a2298e92f200bad9391592eddb7
Silvio Limeira <sicemal@gmail.com>
2023-02-07 10:21:58 -0400

    Backend project created

---
+++ backend/src/test/java/com/limeira/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limeira.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
