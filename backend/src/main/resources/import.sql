f5aedcaf6c4bcea0dcddb4b65f5dda828ebcca35
Silvio Limeira <sicemal@gmail.com>
2023-02-08 08:11:21 -0400

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,36 @@
 INSERT INTO tb_category(name, created_At) VALUES('Books', NOW());
 INSERT INTO tb_category(name, created_At) VALUES('Eletronics', NOW());
-INSERT INTO tb_category(name) created_At) VALUES('Computers', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers', NOW());
+
+INSERT INTO tb_category(name, created_At) VALUES('Books 10', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 11', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 12', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 13', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 14', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 15', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 16', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 17', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 18', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 19', NOW());
+
+INSERT INTO tb_category(name, created_At) VALUES('Books 20', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 21', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 22', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 23', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 24', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 25', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 26', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 27', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 28', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 29', NOW());
+
+INSERT INTO tb_category(name, created_At) VALUES('Books 30', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 31', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 32', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 33', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 34', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 35', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 36', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Eletronics 37', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Computers 38', NOW());
+INSERT INTO tb_category(name, created_At) VALUES('Books 39', NOW());
