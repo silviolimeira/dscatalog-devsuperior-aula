86cc5b96c3bf6db0931b5a639c039a6904d7887f
Silvio Limeira <sicemal@gmail.com>
2023-02-12 11:34:20 -0400

    add field messages in ResourceExeption ValidationError

--- backend/src/main/java/com/limeira/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/limeira/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -60,14 +61,18 @@ public class ResourceExceptionHandler {
 
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		StandardError err = new StandardError();
+	public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Validation exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(status).body(err);
 	}
